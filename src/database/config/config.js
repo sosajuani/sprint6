57ab2d1e985f782cf97490130584ffe8524c465d
sosajuani <sosajuanignacio17@gmail.com>
2022-02-21 19:26:22 -0300

    pruebas

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,13 +1,21 @@
 require('dotenv').config()
 module.exports = {
   
+  // "development": {
+  //   username: process.env.DB_USERNAME,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_DATABASE,
+  //   host: process.env.DB_HOST,
+  //   port: process.env.DB_PORT,
+  //   dialect: process.env.DB_DIALECT,
+  // }
   "development": {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    username: "cthuijo5c3ogyirj",
+    password: "oboivky2ga1ww9lb",
+    database: "mcktikw1woyficanE",
+    host: "uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    dialect: mysql,
   }
 
 }
